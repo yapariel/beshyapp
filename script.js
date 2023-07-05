c9ee03b99f43965a7ae255e196c441e31a0f2a65
yapariel <johnarielyap143@gmail.com>
2023-07-04 22:19:04 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const textInput = document.getElementById("textInput");
+const outputDiv = document.getElementById("output");
+const copyBtn = document.getElementById("copyBtn");
+
+textInput.addEventListener("input", updateOutput);
+
+copyBtn.addEventListener("click", copyText);
+
+function updateOutput() {
+  const inputText = textInput.value;
+  const replacedText = inputText.replace(/ /g, "🤸");
+  outputDiv.innerText = replacedText;
+}
+
+function copyText() {
+  const range = document.createRange();
+  range.selectNode(outputDiv);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+  alert("Text copied to clipboard!");
+}
